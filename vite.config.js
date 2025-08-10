793a78fdbfc015dba3c9a45fe65a8ce52c61fe84
Ahmed Adel <ahmed45adel@yahoo.com>
2025-08-10 10:15:02 +0300

    fetch vite base url from env

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
 		port: 3000,
 		proxy: {
 			"/api": {
-				target: "https://chat-app-backend-one-lyart.vercel.app",
+				target: import.meta.env.VITE_API_URL,
 			},
 		},
 	},
