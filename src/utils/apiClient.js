ba7d3fdd66f346c1f82162230aaef4b599cdb0ae
Ahmed Adel <ahmed45adel@yahoo.com>
2025-08-12 20:00:56 +0300

    trigger deployment

--- src/utils/apiClient.js
+++ src/utils/apiClient.js
@@ -2,9 +2,7 @@ import axios from "axios";
 import Cookies from 'js-cookie';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.MODE === "development"
-    ? "http://localhost:5000"
-    : `${import.meta.env.VITE_API_BASE_URL}`,
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true, // send cookies to the server
 });
 
