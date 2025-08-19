e2590d8dfda678ac58a43e504025a3eaa8146ee9
Ahmed Adel <ahmed45adel@yahoo.com>
2025-08-19 12:48:14 +0300

    fix message breaking ui

--- src/zustand/useConversation.js
+++ src/zustand/useConversation.js
@@ -1,10 +1,17 @@
 import { create } from "zustand";
 
 const useConversation = create((set) => ({
-	selectedConversation: null,
-	setSelectedConversation: (selectedConversation) => set({ selectedConversation }),
-	messages: [],
-	setMessages: (messages) => set({ messages }),
+  selectedConversation: null,
+
+  setSelectedConversation: (selectedConversation) =>
+    set({ selectedConversation }),
+
+  messages: [],
+
+  setMessages: (msgs) =>
+    set({
+      messages: Array.isArray(msgs) ? msgs : msgs ? [msgs] : [], 
+    }),
 }));
 
 export default useConversation;
